7b63bae00201070c74d5730179302f0527f07fc2
Timo Varila <timo.varila@siili.com>
2019-09-24 20:15:43 +0300

    day 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import colorData from "./data/color-data.json";
+import ColorList from "./components/ColorList";
+import AddColorForm from './components/AddColorForm';
+import { v4 } from 'uuid';
 
-function App() {
+export default function App() {
+  const [colors, setColors] = useState(colorData);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AddColorForm 
+        onNewColor={(title, color) => {
+          const newColors = [
+            ...colors,
+            {
+              id: v4(),
+              rating: 0,
+              title,
+              color
+            }
+          ];
+          setColors(newColors);
+        }}
+      />
+      <ColorList
+        colors={colors}
+        onRateColor={(id, rating) => {
+          const newColors = colors.map(color =>
+            color.id === id ? { ...color, rating } : color
+          );
+          setColors(newColors);
+        }}
+        onRemoveColor={id => {
+          const newColors = colors.filter(color => color.id !== id);
+          setColors(newColors);
+        }}
+      />
+    </>
   );
 }
-
-export default App;
